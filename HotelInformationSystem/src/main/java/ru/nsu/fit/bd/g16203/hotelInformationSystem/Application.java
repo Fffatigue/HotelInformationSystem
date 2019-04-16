cc461769b195b8a4513b69d62c9a2716d9747474
a.chmil <a.chmil@g.nsu.ru>
2019-04-16 21:45:05 +0700

    initial commit

---
+++ HotelInformationSystem/src/main/java/ru/nsu/fit/bd/g16203/hotelInformationSystem/Application.java
@@ -0,0 +1,12 @@
+package ru.nsu.fit.bd.g16203.hotelInformationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ HotelInformationSystem/src/main/java/ru/nsu/fit/bd/g16203/hotelInformationSystem/Greeting.java
@@ -0,0 +1,20 @@
+package ru.nsu.fit.bd.g16203.hotelInformationSystem;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
